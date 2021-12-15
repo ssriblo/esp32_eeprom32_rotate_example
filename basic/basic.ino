a437afdd7c4749c4bf86a8a49a4ccfb156e76c6a
sskriblo <a.skriblo@gmail.com>
2021-12-15 15:58:47 +0300

    fix for write_and_commit() call

--- basic/basic.ino
+++ basic/basic.ino
@@ -4,7 +4,10 @@
 
 #include "EEPROM32_Rotate.h"
 
-#define PARTITION_SIZE  4096
+//#define PARTITION_SIZE  4096
+//#define PARTITION_OFFSET    0xFF0
+#define PARTITION_SIZE      0x40 /* 32 for data; 16 for magic byte+crc; 16 spare */
+#define PARTITION_OFFSET    0x30 /* 48+16=64 */
 #define SLOTN           4
 #define MAGICVALR       0x55332299
 
@@ -20,6 +23,7 @@ typedef struct eeprom_data_str{
 } datar_t;
 const datar_t datar_def = { {0,0,0,0}, 0,0, MAGICVALR, 0 };
 datar_t datar;
+uint8_t* p_datar = (uint8_t*) & datar;
 
 uint32_t eeprom32_get_counter(uint8_t ind, uint32_t defaultValue=0) {
     uint32_t ret = defaultValue;
@@ -42,11 +46,11 @@ void rotate_setup(bool isPrint) {
     // Or add them by subtype (it will search and add all partitions with that subtype)
     EEPROMr.add_by_subtype(0x99);
     // Offset where the magic bytes will be stored (last 16 bytes block)
-    EEPROMr.offset(0xFF0);
+    EEPROMr.offset(PARTITION_OFFSET);
     // Look for the most recent valid data and populate the memory buffer
     EEPROMr.begin(PARTITION_SIZE);
     // -------------------------------------------------------------------------
-    uint32_t size =  EEPROMr.readBytes(0, &datar, sizeof(datar));
+    uint32_t size =  EEPROMr.readBytes(0, p_datar, sizeof(datar));
     if(isPrint == true) {
         Serial.println("COUNTERs are:");
         for(int i=0; i<SLOTN; i++) {
@@ -56,13 +60,13 @@ void rotate_setup(bool isPrint) {
         Serial.printf("TIMESTAMP: 0x%08X\n", datar.timestamp);
     }
     if(MAGICVALR != datar.magic_val){
-        memcpy(&datar, &datar_def, sizeof(datar));
+        memcpy(p_datar, &datar_def, sizeof(datar));
         Serial.println("***** WARNING ***** Write default to EEPROM");
     }
     // How to use:
     //nn = datar.value[j];
     //datar.value[j] = 123;
-    //Serial.printf("Commit %s\n", EEPROMr.write_and_commit(&datar, sizeof(datar), PARTITION_SIZE) ? "OK" : "KO");
+    Serial.printf("Commit %s\n", EEPROMr.write_and_commit(p_datar, sizeof(datar), PARTITION_SIZE) ? "OK" : "KO");
     Serial.printf("[EEPROM] Reserved partitions : %u\n", EEPROMr.size());
     Serial.printf("[EEPROM] Partition size      : %u\n", EEPROMr.length());
     Serial.printf("[EEPROM] Parititions in use  : ");
@@ -80,8 +84,8 @@ void setup() {
     // Init DEBUG --------------------------------------------------------------
     Serial.begin(115200);
     delay(500);
-    rotate_setup(false);
-//    rotate_setup(true);
+//    rotate_setup(false);
+    rotate_setup(true);
 }
 
 void loop() {}
